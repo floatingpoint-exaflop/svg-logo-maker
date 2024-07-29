f9dd6a536ac79c09d5da96dfac8819877683fd00
Frederico Swarthings <timscallon1@gmail.com>
2024-07-29 17:36:46 -0500

    Added some comments and fixed a serious bug where the program was only rendering circles. Added example of one of each shape to project files so users can inspect the output code themselves

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {Circle, Square, Triangle} = require("./lib/shapes")
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
 //question colors stuff
+//This is a list of every named html color; we can prevent the user from entering color keywords that are invalid to pass directly into the template literal.
 const htmlNamedColors = [
     "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue",
     "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk",
@@ -24,11 +25,15 @@ const htmlNamedColors = [
     "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white",
     "whitesmoke", "yellow", "yellowgreen"
   ];
+//just in case the user types crazy, we'll normalize the text as lower case.
 const isNamedColor = (color) => htmlNamedColors.includes(color.toLowerCase());
-const isHexColor = (hex) => /^#([0-9A-F]{3}){1,2}$/i.test(hex);
-// Regular Expression to ensure a proper hex code, starting with a # and including 3 or 6 numbers or letters A thru F (hex codes can be 3 char too and still be valid). The letters can be upper or lowercase, though hexcodes themselves are not case-sensitive anyway; it just helps standardize the inputs better.
 
-//questions
+const isHexColor = (hex) => /^#([0-9A-F]{3}){1,2}$/i.test(hex);
+// Regular Expression to ensure a proper hex code, starting with a # and including 3 or 6 numbers or letters A thru F (hex codes can be 3 char too and still be valid). 
+//The letters can be upper or lowercase (hence the i), though hexcodes themselves are not case-sensitive anyway; the user can type them however they like as long as the format is valid.
+
+
+//questions; note that for colors, the user is unable to enter anything that isn't a valid hex or html named color.
 function userShapeQ(){
     return [
     {
@@ -67,15 +72,17 @@ function userShapeQ(){
     }
  ]};
 
+
+//The main function: this brings in the questions and calls the correct class constructor for the chosen shape, which is created with the other chosen properties.
 async function init(){
     const questions = userShapeQ();
     const answers = await inquirer.prompt(questions);
     let shape;
-    if (answers.shape = "Circle"){
+    if (answers.shape === "Circle"){
         shape = new Circle(answers.shapecolor, answers.text, answers.textcolor)
-    } else if (answers.shape = "Triangle"){
+    } else if (answers.shape === "Triangle"){
         shape = new Triangle(answers.shapecolor, answers.text, answers.textcolor)
-    } else if (answers.shape = "Square"){
+    } else if (answers.shape === "Square"){
         shape = new Square(answers.shapecolor, answers.text, answers.textcolor)
     }
     const outputSVG = shape.render();
